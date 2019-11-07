e0636198f6bb62788074e3c9140404d511fe1134
jihye <zehye.01@gmail.com>
2019-11-07 15:25:04 +0900

    wrap compareing statement with function

--- numguess.py
+++ numguess.py
@@ -4,11 +4,17 @@ answer = randint(1,101)
 print(answer)
 
 chance = 3
-for _ in range(chance):
-	guess =int(input("1~100까지 숫자를 고르세요:"))
 
-	if guess == answer:
-		print("correct")
-		break
-	else:
-		print("wrong!")
+def compare_guess():
+	for _ in range(chance):
+		guess =int(input("1~100까지 숫자를 고르세요:"))
+
+		if guess == answer:
+			print("correct")
+			break
+		else:
+			print("wrong!")
+
+
+
+compare_guess()
