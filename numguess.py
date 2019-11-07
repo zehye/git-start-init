399e7c0f50b353d060e5da9081d08237267a4886
jihye <zehye.01@gmail.com>
2019-11-07 15:20:48 +0900

    add for loop to repeat a few times to try!

--- numguess.py
+++ numguess.py
@@ -3,9 +3,12 @@ from random import randint
 answer = randint(1,101)
 print(answer)
 
-guess =int(input("1~100까지 숫자를 고르세요:"))
+chance = 3
+for _ in range(chance):
+	guess =int(input("1~100까지 숫자를 고르세요:"))
 
-if guess==answer:
-	print(">>>>>맞았습니다")
-else:
-	print(">>>>>틀렸지롱")
+	if guess == answer:
+		print("correct")
+		break
+	else:
+		print("wrong!")
